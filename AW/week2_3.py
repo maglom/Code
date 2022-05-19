42128881bb4ca84971bb61218fca97a82a8cfef2
maglom <magnuslomsdalen@gmail.com>
2022-05-19 14:51:31 +0200

    Endring

--- AW/week2_3.py
+++ AW/week2_3.py
@@ -3,13 +3,14 @@
 dic = {}
 
 while True:
-    userinput = input("If you want to print the dictonary enter PRINT NOW \nEnter a key-value pair: ")
-    if len(userinput.split()) != 2:
+    ls = input("If you want to print the dictonary enter PRINT NOW \nEnter a key-value pair: ")
+    ls = ls.split()
+    if len(ls) != 2:
         print("Only two words are accepted")
-    elif userinput == "PRINT NOW":
+    elif ls[0] == "PRINT" and ls[1] == "NOW":
         print(dic)
-    elif userinput[0] not in dic:
-        dic[userinput[0]] = list(userinput[1])
+    elif ls[0] not in dic:
+        dic[ls[0]] = [ls[1]]
     else:
-        dic[userinput[0]] = dic[userinput[0]].append(userinput[1])
+        dic[ls[0]] = dic[ls[0]] + [ls[1]]
     
