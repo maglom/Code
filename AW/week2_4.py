c28d27cf3f1def137dd225f152d476936c1f6909
maglom <magnuslomsdalen@gmail.com>
2022-05-20 11:30:10 +0200

    ja

---
+++ AW/week2_4.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+dic = defaultdict(list)
+
+while True:
+    ls = input("If you want to print the dictonary enter PRINT NOW \nEnter a key-value pair: ")
+    ls = ls.split()
+    if len(ls) != 2:
+        print("Only two words are accepted")
+    elif ls[0] == "PRINT" and ls[1] == "NOW":
+        print(dic)
+        print("To end the program press ctrl+c")
+    elif ls[0] not in dic:
+        dic[ls[0]] = [ls[1]]
+    else:
+        dic[ls[0]] = dic[ls[0]] + [ls[1]]
+        
