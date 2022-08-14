2793aaff92dd19a94705a70d1b25420f7166ea87
maglom <96472557+maglom@users.noreply.github.com>
2022-08-14 09:16:03 +0200

    laget app

--- AW/dash/plot/expl.py
+++ AW/dash/plot/expl.py
@@ -4,10 +4,6 @@ import plotly.express as px
 
 df = pd.read_csv(r'C:\Users\magnu\Documents\Code\AW\dash\plot\airline_passenger_satisfaction.csv')
 
-df[:30]
-
-df['Gender'][:30]
-
 fig = px.line(df, x=df['ID'][:300], y=df['Seat Comfort'][:300])
 
 fig.show()
